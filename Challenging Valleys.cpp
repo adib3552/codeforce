c323f1952210e35d97ef246071abfec5a23dcbfe
Adib <adib3552@gmail.com>
2023-10-22 01:25:33 +0600

    Challenging Valleys

--- Challenging Valleys.cpp
+++ Challenging Valleys.cpp
@@ -1,15 +1,34 @@
 #include<bits/stdc++.h>
+#define ll long long int
+const ll N=1e10+10;
 using namespace std;
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
+        ll n,temp=0,c=0;
         cin>>n;
-        int arr[n];
+        vector<ll>v;
+        v.push_back(N);
         for(int i=0; i<n; i++){
-            cin>>arr[i];
+            ll x;
+            cin>>x;
+            if(x!=temp){
+                v.push_back(x);
+                temp=x;
+            }
         }
-
+        v.push_back(N);
+        for(int i=1; i<v.size()-1; i++){
+            if(v[i]<v[i-1] && v[i]<v[i+1]){
+                c++;
+            }
+        }
+        if(c>1){
+            cout<<"NO\n";
+        }
+        else{
+            cout<<"YES\n";
+        }
     }
 }
